4bd695173422a522a439aee9f2d303f4b4ed15a0
E Shifrin <eshifrin@gmail.com>
2017-03-14 19:34:51 -0700

    front end skeleton

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.static(__dirname + '/../client/dist'));
 
 app.get('/', function (req, res) {
   res.send('Hello world');
