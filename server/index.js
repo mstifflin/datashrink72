301fd627ffb5a74316cf3a0f3a818b8f6e8148fe
Tiffany <tiffanylin27@gmail.com>
2017-03-15 14:50:42 -0700

    Implement Twitter login functionality

--- server/index.js
+++ server/index.js
@@ -6,12 +6,12 @@ var API = require('./API_KEYS.js');
 var passport = require('passport');
 var ensureLogIn = require('connect-ensure-login');
 var FacebookStrat = require('passport-facebook').Strategy;
+var TwitterStrat = require('passport-twitter').Strategy;
 
 var app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
 
-app.set('testViews', __dirname + '/testViews');
 app.set('view engine', 'ejs');
 
 passport.use(new FacebookStrat({
@@ -20,12 +20,25 @@ passport.use(new FacebookStrat({
     callbackURL: 'http://localhost:3000/facebook/return'
   },
   function(accessToken, refreshToken, profile, cb) {
-    console.log(accessToken);
-    console.log(refreshToken);
-    console.log(profile);
+    console.log('ACCESS: ', accessToken);
+    console.log('REFRESH: ', refreshToken);
+    console.log('PROFILE: ', profile);
     return cb(null, profile);
   }
 ));
+
+passport.use(new TwitterStrat({
+    consumerKey: API.twitterKey,
+    consumerSecret: API.twitterSecret,
+    callbackURL: 'http://127.0.0.1:3000/twitter/return'
+  },
+  function(token, tokenSecret, profile, cb) {
+    console.log('TOKEN: ', token);
+    console.log('SECRET: ', tokenSecret);
+    console.log('PROFILE: ', profile);
+
+    return cb(null, profile);
+  }));
 
 //TODO: 
 // Configure Passport authenticated session persistence.
@@ -47,6 +60,7 @@ passport.deserializeUser(function(obj, cb) {
 
 
 app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressSession({secret: 'keyboard cat', resave: true, saveUnitialized: true}));
 
@@ -69,7 +83,22 @@ app.get('/facebook/return',
 app.get('/facebookProfile', 
   ensureLogIn.ensureLoggedIn(),
   function(req, res) {
-    res.render('facebookProfile', { user: req.user });
+    res.render('testProfile', { user: req.user });
+  });
+
+app.get('/twitter', passport.authenticate('twitter'));
+
+app.get('/twitter/return', 
+  passport.authenticate('twitter', { failureRedirect: '/'}),
+  function(req, res) {
+    // TODO: redirect to data analysis
+    res.redirect('/twitterProfile');
+  });
+
+app.get('/twitterProfile',
+  ensureLogIn.ensureLoggedIn(),
+  function(req, res) {
+    res.render('testProfile', { user: req.user });
   });
 
 app.listen(3000, function() {
