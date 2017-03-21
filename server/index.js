7d46c5f1d6a07dc0b8fedd022303048599a48b64
jdbriner07 <jdbriner07@gmail.com>
2017-03-21 09:37:27 -0700

    changed to detect heroku port

--- server/index.js
+++ server/index.js
@@ -89,6 +89,6 @@ app.get('/*', function(req, res) {
   res.send('in progress');
 })
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('Listening on port 3000.');
 });
