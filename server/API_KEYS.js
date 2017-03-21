40f0b7cb6b5c2e18e257497083b0f1c4dba592bc
jdbriner07 <jdbriner07@gmail.com>
2017-03-21 09:18:45 -0700

    commit before starting heroku server

---
+++ server/API_KEYS.js
@@ -0,0 +1,7 @@
+var express = require('express');
+
+module.exports.facebookID = 796927700457790; // This is the app ID
+module.exports.facebookSecret = 'f7d9b3cc4fea68bfc1a06e219aa1c19f'; // This is the app secret
+
+module.exports.twitterKey = '4IPU5FHMvj0WmtnuhvxtyZm6O';
+module.exports.twitterSecret = 'yft5zHCE4pjxIWdtoCCaapSPL7fRqos3blZNNZxtH1ZXaB1m95';
