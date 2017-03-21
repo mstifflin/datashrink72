95e20e52309ba6d4131952cf7e57cc2307294db8
jdbriner07 <jdbriner07@gmail.com>
2017-03-21 09:30:13 -0700

    api keys needed for heroku

--- server/API_KEYS.js
+++ server/API_KEYS.js
@@ -3,5 +3,5 @@ var express = require('express');
 module.exports.facebookID = 796927700457790; // This is the app ID
 module.exports.facebookSecret = 'f7d9b3cc4fea68bfc1a06e219aa1c19f'; // This is the app secret
 
-module.exports.twitterKey = '4IPU5FHMvj0WmtnuhvxtyZm6O';
-module.exports.twitterSecret = 'yft5zHCE4pjxIWdtoCCaapSPL7fRqos3blZNNZxtH1ZXaB1m95';
+module.exports.twitterKey = '3AgtiXGQxmljq8Wy4ofJGhU6h'; // consumer key
+module.exports.twitterSecret = 'h70NrluR9oDGcKBflt2Fl6fiMJjhvztSwW1mlPpc8BgySTooIU'; //consumer secret
