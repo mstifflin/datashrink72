63ee4ad0d16872765cb62c4b3911b2f3354072e8
Jeffrey Briner <jdbriner07@gmail.com>
2017-03-21 13:56:46 -0700

    Delete API_KEYS.js

--- server/API_KEYS.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-
-module.exports.facebookID = 796927700457790; // This is the app ID
-module.exports.facebookSecret = 'f7d9b3cc4fea68bfc1a06e219aa1c19f'; // This is the app secret
-
-module.exports.twitterKey = '3AgtiXGQxmljq8Wy4ofJGhU6h'; // consumer key
-module.exports.twitterSecret = 'h70NrluR9oDGcKBflt2Fl6fiMJjhvztSwW1mlPpc8BgySTooIU'; //consumer secret
