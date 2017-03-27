36681f34ac4871b62274a172f1e8d166858682f8
Tiffany <tiffanylin27@gmail.com>
2017-03-27 14:21:50 -0700

    Change type of email in sign up to email, code clean up

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -23,36 +23,24 @@ class SignUpForm extends React.Component {
     this.setState({[name]: e.target.value});
   }
 
-  validateEmail() {
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(this.state.email);
-  }
-
   sendForm(event) {
     event.preventDefault();
-    if (!this.validateEmail()) {
-      this.setState({
-        error: true,
-        errorMessage: 'Please enter a valid email address.'
-      });
-    } else {
-      s.serverPost('signup', this.state).then(e => {
-        if (e.data.username) {
-          this.props.update(e.data.username);
-        }
-        if (e.data.error) {
-          this.setState({
-            error: true,
-            errorMessage: e.data.error
-          });
-        }
-      }).catch(e => {
+    s.serverPost('signup', this.state).then(e => {
+      if (e.data.username) {
+        this.props.update(e.data.username);
+      }
+      if (e.data.error) {
         this.setState({
           error: true,
-          errorMessage: 'There was a server error. Please wait then try again.'
-        })
-      }); 
-    }
+          errorMessage: e.data.error
+        });
+      }
+    }).catch(e => {
+      this.setState({
+        error: true,
+        errorMessage: 'There was a server error. Please wait then try again.'
+      })
+    }); 
   }
 
   render () {
@@ -72,7 +60,7 @@ class SignUpForm extends React.Component {
         </label>
         <label>
           Email:
-          <input type="text" name='email' onChange={this.updateFormValue} defaultValue=''/>
+          <input type="email" name='email' onChange={this.updateFormValue} defaultValue=''/>
         </label>
         <input type="submit" className="submit" defaultValue ='submit' />
       </form>
